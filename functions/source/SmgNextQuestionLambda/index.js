fe55a343da85f694f9512cf67805e2b9451bfeb5
Ivan Bliskavka <ivan.bliskavka@voicefoundry.com>
2019-04-11 16:02:43 -0400

    format

--- functions/source/SmgNextQuestionLambda/index.js
+++ functions/source/SmgNextQuestionLambda/index.js
@@ -7,8 +7,8 @@ exports.handler = (event, context, callback) => {
         return;
     }
     console.log(JSON.stringify(event));
-    if(event.body !== undefined){
-        
+    if (event.body !== undefined) {
+
         var bodyObj = JSON.parse(event.body);
         event.Details = bodyObj.Details;
     }
@@ -18,49 +18,50 @@ exports.handler = (event, context, callback) => {
     config.previousQuestionPrefixType = event.Details.Parameters.PreviousQuestionPrefixType;
     config.previousResponse = event.Details.Parameters.PreviousResponse;
     config.questionCount = parseInt(event.Details.Parameters.QuestionCount, 10) + 1;
-    config.validResponseValues =  event.Details.Parameters.ValidResponseValues;
-    config.maxQuestionRetries =  parseInt(event.Details.Parameters.MaxQuestionRetries, 10);
-    config.timeBeforeAnswer =  parseInt(event.Details.Parameters.TimeBeforeAnswer, 10);
-    config.surveyProgressDetail =  JSON.parse(event.Details.Parameters.SurveyProgressDetail);
-    if (!(config.currentAnswer) )
+    config.validResponseValues = event.Details.Parameters.ValidResponseValues;
+    config.maxQuestionRetries = parseInt(event.Details.Parameters.MaxQuestionRetries, 10);
+    config.timeBeforeAnswer = parseInt(event.Details.Parameters.TimeBeforeAnswer, 10);
+    config.surveyProgressDetail = JSON.parse(event.Details.Parameters.SurveyProgressDetail);
+    if (!(config.currentAnswer))
         config.currentAnswer = '';
     config.callback = callback;
     config.context = context;
-    if((config.validResponseValues.indexOf(config.currentAnswer) == -1 && config.questionCount < config.maxQuestionRetries) || (answerQuestionDiff <  config.timeBeforeAnswer && config.questionCount < config.maxQuestionRetries)){
-        config.response = JSON.parse( config.previousResponse);
+    if ((config.validResponseValues.indexOf(config.currentAnswer) == -1 && config.questionCount < config.maxQuestionRetries) || (answerQuestionDiff < config.timeBeforeAnswer && config.questionCount < config.maxQuestionRetries)) {
+        config.response = JSON.parse(config.previousResponse);
         config.response.questionCount = config.questionCount;
-        config.response.previousResponse =  config.previousResponse;
+        config.response.previousResponse = config.previousResponse;
         config.response.questionTimestamp = Date.now();
-        config.response.questionTimestamp =  config.response.questionTimestamp + (config.timeBeforeAnswer*2);
+        config.response.questionTimestamp = config.response.questionTimestamp + (config.timeBeforeAnswer * 2);
         config.response.wrongAnswer = true;
-        config.surveyProgressDetail.splice(-1,1);
-        updateSurveyProgress(1, config.response.nextQuestion, config.currentAnswer,  config.surveyProgressDetail);
-        if( config.currentAnswer == 'Timeout' || answerQuestionDiff <  config.timeBeforeAnswer){
-             config.response.wrongAnswer = false;
-             config.response.questionTimestamp =  config.response.questionTimestamp - (config.timeBeforeAnswer*2);
+        config.surveyProgressDetail.splice(-1, 1);
+        updateSurveyProgress(1, config.response.nextQuestion, config.currentAnswer, config.surveyProgressDetail);
+        if (config.currentAnswer == 'Timeout' || answerQuestionDiff < config.timeBeforeAnswer) {
+            config.response.wrongAnswer = false;
+            config.response.questionTimestamp = config.response.questionTimestamp - (config.timeBeforeAnswer * 2);
         }
-        
+
         config.response.surveyProgressDetail = JSON.stringify(config.surveyProgressDetail);
-        
+
         console.log('LAMBDA RETURN IS:');
         console.log(config.response);
 
         config.callback(null, JSON.parse(config.response));
-        config.context.succeed();  
-    }
-    else{
+        config.context.succeed();
+    } else {
         getNextQuestion(config);
     }
- };
-function updateSurveyProgress(questionId, questionText, answerInput, progressObj){
+};
+
+function updateSurveyProgress(questionId, questionText, answerInput, progressObj) {
     var newQuestion = {
         'questionPrefixType': questionId,
         'questionText': questionText,
         'answerInput': answerInput
     };
     progressObj.push(newQuestion);
-} 
-function getNextQuestion(config){
+}
+
+function getNextQuestion(config) {
     var url = getParms(process.env.SURVEY_URL);
     url = url.replace('SURVEYPROGRESS', config.surveyProgress);
     url = url.replace('CURRENTANSWER', config.currentAnswer);
@@ -70,22 +71,23 @@ function getNextQuestion(config){
 
 
 //https://connect-api.smg.com/LambdaAccess/NextQuestion/71f1250b-3221-11e9-9926-54e1addfa841/SURVEYPROGRESS/CURRENTANSWER/PQUESTIONPREFIXTYPE
-function getHost(url){
+function getHost(url) {
     var a = url.split("/");
-    return  a[2];
+    return a[2];
 }
-function correctPath(url, path){
-    if (path == "_//-1")
-    {
-        return url.replace("Next","First").replace("/SURVEYPROGRESS/CURRENTANSWER/PQUESTIONPREFIXTYPE", "");
+
+function correctPath(url, path) {
+    if (path == "_//-1") {
+        return url.replace("Next", "First").replace("/SURVEYPROGRESS/CURRENTANSWER/PQUESTIONPREFIXTYPE", "");
     }
     var a = url.split("/");
-    return "/" + a.slice(3,6).join('/') + "/" + path;
+    return "/" + a.slice(3, 6).join('/') + "/" + path;
 }
-function getParms(url){
-        var a = url.split("/");
-        var rtn = a.slice(6).join('/');
-        console.log(rtn);
+
+function getParms(url) {
+    var a = url.split("/");
+    var rtn = a.slice(6).join('/');
+    console.log(rtn);
     return rtn;
 }
 
@@ -93,30 +95,30 @@ function getData(path, config) {
 
     var host = getHost(process.env.SURVEY_URL);
     path = correctPath(process.env.SURVEY_URL, path);
-  try {
-    const options = {
-      hostname: host,
-      port: 443,
-      path: path,
-      method: 'POST'
-    };
-    console.log(JSON.stringify(options));
+    try {
+        const options = {
+            hostname: host,
+            port: 443,
+            path: path,
+            method: 'POST'
+        };
+        console.log(JSON.stringify(options));
 
-    const req = https.request(options, (res) => {
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-          config.callback(null, JSON.parse(chunk.toString()));
-      });
-    });
+        const req = https.request(options, (res) => {
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+                config.callback(null, JSON.parse(chunk.toString()));
+            });
+        });
 
-    req.on('error', (e) => {
-      console.error(e);
-      config.callback(e);
-    });
-    req.end();
+        req.on('error', (e) => {
+            console.error(e);
+            config.callback(e);
+        });
+        req.end();
 
-  } catch (e) {
-    console.log("GetData Failed: " + e.message);
-    config.callback(e.message);
-  }
+    } catch (e) {
+        console.log("GetData Failed: " + e.message);
+        config.callback(e.message);
+    }
 }
