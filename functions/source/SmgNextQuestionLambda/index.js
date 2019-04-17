533341d84f27864e3f9ca6229a02f36700dd4f05
tom-huguenard <tom.huguenard@gmail.com>
2019-04-17 11:47:05 -0500

    Move next question lambda business logic to API Improve error handling and recovery

--- functions/source/SmgNextQuestionLambda/index.js
+++ functions/source/SmgNextQuestionLambda/index.js
@@ -2,6 +2,8 @@ var https = require('https');
 
 exports.handler = (event, context, callback) => {
     var config = {};
+    var bad = false;
+    var failEvent = JSON.stringify(event);
 
     if (event.keepwarm) {
         callback(null, 'kept warm');
@@ -15,77 +17,47 @@ exports.handler = (event, context, callback) => {
         var bodyObj = JSON.parse(event.body);
         event.Details = bodyObj.Details;
     }
-
-    var answerQuestionDiff = Date.now() - parseInt(event.Details.Parameters.QuestionTimestamp, 10);
-    config.currentAnswer = event.Details.Parameters.CurrentAnswer;
-    config.surveyProgress = event.Details.Parameters.SurveyProgress;
-    config.previousQuestionPrefixType = event.Details.Parameters.PreviousQuestionPrefixType;
-    config.previousResponse = event.Details.Parameters.PreviousResponse;
-    config.questionCount = parseInt(event.Details.Parameters.QuestionCount, 10) + 1;
-    config.validResponseValues = event.Details.Parameters.ValidResponseValues;
-    config.maxQuestionRetries = parseInt(event.Details.Parameters.MaxQuestionRetries, 10);
-    config.timeBeforeAnswer = parseInt(event.Details.Parameters.TimeBeforeAnswer, 10);
-    config.surveyProgressDetail = JSON.parse(event.Details.Parameters.SurveyProgressDetail);
-
-    if (!(config.currentAnswer)){
-        config.currentAnswer = '';
+    
+    if (!event.Details){
+        event.Details = {};
+        bad = true;
     }
-        
-    config.callback = callback;
-    config.context = context;
-
-    if ((config.validResponseValues.indexOf(config.currentAnswer) == -1 && config.questionCount < config.maxQuestionRetries) || (answerQuestionDiff < config.timeBeforeAnswer && config.questionCount < config.maxQuestionRetries)) {
-        config.response = JSON.parse(config.previousResponse);
-        config.response.questionCount = config.questionCount;
-        config.response.previousResponse = config.previousResponse;
-        config.response.questionTimestamp = Date.now();
-        config.response.questionTimestamp = config.response.questionTimestamp + (config.timeBeforeAnswer * 2);
-        config.response.wrongAnswer = true;
-        config.surveyProgressDetail.splice(-1, 1);
-        
-        updateSurveyProgress(1, config.response.nextQuestion, config.currentAnswer, config.surveyProgressDetail);
-
-        if (config.currentAnswer == 'Timeout' || answerQuestionDiff < config.timeBeforeAnswer) {
-            config.response.wrongAnswer = false;
-            config.response.questionTimestamp = config.response.questionTimestamp - (config.timeBeforeAnswer * 2);
-        }
-
-        config.response.surveyProgressDetail = JSON.stringify(config.surveyProgressDetail);
-
-        console.log('LAMBDA RETURN IS:');
-        console.log(config.response);
-
-        config.callback(null, JSON.parse(config.response));
-        config.context.succeed();
-    } else {
-        exports.sendData(config);
+    if (!event.Details.Parameters){
+        event.Details.Parameters = { badRequest: true};
+        bad = true;
     }
+
+    if (!event.Details.ContactData || !event.Details.ContactData.ContactId){
+        event.Details.ContactData = { badRequest: true, ContactId : "badRequest"};
+        bad = true;
+    }
+    if (!event.Details.ContactData.Attributes){
+        event.Details.ContactData.Attributes = { badRequest: true};
+        bad = true;
+    }    
+    
+    var ev = {
+        ApiKey: process.env.API_KEY,
+        ContactId: event.Details.ContactData.ContactId,
+        Parameters: JSON.stringify(event.Details.Parameters),
+        Attributes: JSON.stringify((event.Details.ContactData.Attributes))
+    };
+    
+    if (bad){
+        ev.Event = failEvent;
+    }
+    
+    console.log(JSON.stringify(ev));
+    exports.sendData(ev, context, callback);
 };
 
-function updateSurveyProgress(questionId, questionText, answerInput, progressObj) {
-    var newQuestion = {
-        'questionPrefixType': questionId,
-        'questionText': questionText,
-        'answerInput': answerInput
-    };
-    progressObj.push(newQuestion);
-}
 
-
-exports.sendData = (config) => {
-
-    var dto = JSON.stringify({
-        apiKey: process.env.API_KEY,
-        surveyProgress: config.surveyProgress,
-        currentAnswer: config.currentAnswer,
-        previousQuestionPrefixType : config.previousQuestionPrefixType,
-        surveyDetail: ''
-    });
-    
+exports.sendData = (reqData, context, callback) => {
+    var dto = JSON.stringify(reqData);
     try {
         const options = {
             hostname: process.env.API_HOST,
-            path: '/LambdaAccess/v2/NextQuestion',
+            path: '/LambdaAccess/v3/NextQuestion',
             port: 443,
             method: 'POST',
             headers: {
@@ -102,13 +74,13 @@ exports.sendData = (config) => {
 
             res.on('data', (d) => {
                 console.log('Response: ' + d.toString());
-                config.callback(null, JSON.parse(d.toString()));
+                callback(null, JSON.parse(d.toString()));
             });
         });
 
         req.on('error', (e) => {
             console.error('error:', e);
-            config.callback(e);
+            callback(e);
         });
 
         req.write(dto);
@@ -116,6 +88,6 @@ exports.sendData = (config) => {
 
     } catch (e) {
         console.log("GetData Failed: " + e.message);
-        config.callback(e.message);
+        callback(e.message);
     }
 }
